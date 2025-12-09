49b634c8271f0a682f843eaaf87db113b82b56a5
Rampravesh <rampraveshkr4545@gmail.com>
2025-12-10 02:10:00 +0530

    add Chat ,Group and Login pages  and import in app.jsx with code-splitting  for lazy loding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const Home = lazy(() => import("./pages/Home.jsx"));
 const About = lazy(() => import("./pages/About.jsx"));
+const Chat = lazy(() => import("./pages/Chat.jsx"));
+const Group = lazy(() => import("./pages/Group.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/group" element={<Group />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
